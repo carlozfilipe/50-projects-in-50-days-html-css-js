f7110b7cd5adb2fed37a024d9c67ff2b448f9312
Carlos Filipe <thecarlosfilipe@gmail.com>
2024-04-23 23:30:55 -0300

    🍻 Day 10: Dad Jokes

--- day-10-dad-jokes/script.js
+++ day-10-dad-jokes/script.js
@@ -1,1 +1,35 @@
-console.log('JavaScript running!!!');
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+// Usando .then()
+/* function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+} */
+
+// Usando async/await
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    }
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config);
+  const data = await res.json();
+
+  jokeEl.innerHTML = data.joke;
+}
+
+generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
